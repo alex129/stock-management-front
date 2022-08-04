629478e04c9ee78bbf35220799d5e8f5b38775ab
alejandro <AlejP2121>
2022-08-04 14:43:25 +0200

    list of products and stock management

---
+++ src/components/Products.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref, onMounted, reactive } from "vue";
+import axios from "axios";
+
+interface Product {
+     id: number;
+     name: string;
+     quantity: number;
+}
+
+defineProps({
+     msg: String,
+});
+
+const state = reactive<{ products: Product[], quantities: number[] }>({
+     products: [],
+     quantities: []
+});
+
+onMounted(() => {
+     fetchData();
+});
+
+const fetchData = () => {
+     axios.get("http://pdpaola.backend.test/api/products")
+          .then((res) => {
+               console.log(res.data);
+               state.products = res.data;
+          })
+          .catch((err) => {
+               console.log(err);
+          });
+};
+
+const addMovemement = (product: Product, quantity) => {
+     axios.post("http://pdpaola.backend.test/api/movements", {product_id: product.id, quantity}).then((res) => {
+               console.log(res.data);
+               fetchData();
+          })
+          .catch((err) => {
+               console.log(err);
+          });
+};
+</script>
+
+<template>
+     <div class="container">
+          <h3>Products</h3>
+          <table class="table table-bordered table-striped table-sm">
+               <thead>
+                    <tr>
+                         <th>id</th>
+                         <th>Name</th>
+                         <th>Quantity</th>
+                         <th></th>
+                    </tr>
+               </thead>
+               <tbody>
+                    <tr v-for="(product, index) in state.products" :key="product.id">
+                         <td>{{ product.id }}</td>
+                         <td>{{ product.name }}</td>
+                         <td>{{ product.quantity }}</td>
+                         <td>
+                              <div class="d-flex gap-1">
+                                   <input type="number" class="input-control" v-model="state.quantities[index]" />
+                                   <button class="btn btn-sm btn-primary" @click="addMovemement(product, state.quantities[index])">Add stock movement</button>
+                              </div>
+                         </td>
+                    </tr>
+               </tbody>
+          </table>
+     </div>
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(VueAxios, axios)
+app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
